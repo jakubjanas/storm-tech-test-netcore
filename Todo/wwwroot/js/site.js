be223fa6c920557760e57bb39cae6bb99fe46397
Jakub Janas <jakubjanas@econsulting.pl>
2021-08-01 17:11:41 +0200

    Added option to hide done elements on Todolist Detail View

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById("hideDoneItemsBtn")
+    .addEventListener("change", (event) => {
+        var displayStyle = event.currentTarget.checked ? "none" : "block";
+        var doneItems = document.getElementsByClassName("is-done");
+
+        for (var i = 0; i < doneItems.length; i++) {
+            doneItems[0].style.display = displayStyle;
+        }
+    });
