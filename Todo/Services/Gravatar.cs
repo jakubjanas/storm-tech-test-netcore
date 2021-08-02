2911ebd351a67a94ff73e78b9b62ee9b8a0e1e1c
Jakub Janas <jakubjanas@econsulting.pl>
2021-08-02 17:45:31 +0200

    Display data from Gravatar

--- Todo/Services/Gravatar.cs
+++ Todo/Services/Gravatar.cs
@@ -1,10 +1,18 @@
-﻿using System.Security.Cryptography;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Todo.Services
 {
     public static class Gravatar
     {
+        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly string path = "https://www.gravatar.com/{0}.json";
+
         public static string GetHash(string emailAddress)
         {
             using (var md5 = MD5.Create())
@@ -19,6 +27,32 @@ namespace Todo.Services
                 }
                 return builder.ToString().ToLowerInvariant();
             }
+        }
+
+        public static async Task<string> GetName(string emailAddress)
+        {
+            var hash = GetHash(emailAddress);
+            var result = string.Empty;
+
+            try
+            {
+                //https://stackoverflow.com/a/6905471
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "Other");
+
+                var response = await httpClient.GetAsync(string.Format(path, hash));
+                var body = await response.Content.ReadAsStringAsync();
+
+                var jsonResult = JObject.Parse(body);
+                var jtokenResult = jsonResult["entry"].Children().FirstOrDefault();
+
+                result = jtokenResult != null ? jtokenResult["displayName"]?.Value<string>() : "";
+            }
+            catch (Exception ex)
+            {
+                //Log Exception
+            }
+
+            return result;
         }
     }
 }
